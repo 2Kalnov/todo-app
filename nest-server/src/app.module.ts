d31b698a6100775b248e322cce10afb3ae1f8975
nikitakalnov <jdmcorolla21@gmail.com>
2021-01-09 22:56:54 +0300

    Extracted TypeORM config into separate file

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -6,19 +6,7 @@ import {Task} from "./tasks/task.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'admin',
-      password: 'tOOd00$',
-      database: 'todo_app',
-      entities: [Task],
-      migrations: ["migrations/*.ts"],
-      cli: {
-        "migrationsDir": "migrations"
-      }
-    })
+    TypeOrmModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
