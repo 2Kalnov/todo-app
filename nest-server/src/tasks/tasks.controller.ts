87facad648463b4825a14686abd0cc6bd6b8d528
nikitakalnov <jdmcorolla21@gmail.com>
2021-01-09 22:20:32 +0300

    Tasks controller

---
+++ nest-server/src/tasks/tasks.controller.ts
@@ -0,0 +1,31 @@
+import {Body, Controller, Get, Param, Patch, Post} from "@nestjs/common";
+import {TasksService} from "./tasks.service";
+import {Task} from "./task.entity";
+import {CreateTaskDto} from "./dto/CreateTaskDto";
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private readonly tasksService: TasksService) {
+    }
+
+    @Get()
+    async getAll() : Promise<Task[]> {
+        return this.tasksService.findAll();
+    }
+
+    @Get(':id')
+    async get(@Param('id') id: number) : Promise<Task> {
+        return this.tasksService.findById(id);
+    }
+
+    @Post()
+    async create(@Body() taskDto: CreateTaskDto ) : Promise<number> {
+        const createdTask = await this.tasksService.create(taskDto);
+        return createdTask.id;
+    }
+
+    @Patch(':id')
+    setDone(@Param('id') id: number) {
+        return this.tasksService.setDone(id);
+    }
+}
