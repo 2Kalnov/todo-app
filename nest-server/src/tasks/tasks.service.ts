d82b2a52e0071064521638db69947bcd74b6620c
nikitakalnov <jdmcorolla21@gmail.com>
2021-01-09 22:04:36 +0300

    Tasks service

---
+++ nest-server/src/tasks/tasks.service.ts
@@ -0,0 +1,31 @@
+import {Injectable, NotFoundException} from "@nestjs/common";
+import { Task, TaskStatus } from "./task.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {CreateTaskDto} from "./dto/CreateTaskDto";
+
+@Injectable()
+export class TasksService {
+    constructor(@InjectRepository(Task) private tasksRepository: Repository<Task>) {
+    }
+
+    findById(id : number) : Promise<Task> {
+         return this.tasksRepository.findOne(id);
+    }
+
+    findAll(offset: number, limit: number) : Promise<Task[]> {
+        return this.tasksRepository.find({ skip: offset, take: limit });
+    }
+
+    edit(id: number, editInfo: CreateTaskDto)  {
+        this.tasksRepository.update(id, editInfo);
+    }
+
+    async setDone(id: number) {
+        await this.tasksRepository.update(id, { status: TaskStatus.DONE });
+    }
+
+    async delete(id: number) {
+        await this.tasksRepository.delete(id);
+    }
+}
