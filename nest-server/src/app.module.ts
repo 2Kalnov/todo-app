87cc6800cda075f1cdefbdf36a27678d7105d2ea
nikitakalnov <jdmcorolla21@gmail.com>
2021-01-09 21:16:58 +0300

    Setup database connection properties

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'admin',
+      password: 'tOOd00$',
+      database: 'todo_app',
+      entities: [],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
