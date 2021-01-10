d37d5a33396d9c19e6d43a5585fdd9090a84f0ec
2Kalnov <jdmcorolla21@gmail.com>
2021-01-10 21:56:54 +0300

    Bootstrapped React-project for todo-list

---
+++ View/todo-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+type AppProps = {
+
+}
+
+export const App: React.FC<AppProps> = (props: AppProps) => {
+  return (
+    <h2>Заметки</h2>
+  )
+}
+
+
+
