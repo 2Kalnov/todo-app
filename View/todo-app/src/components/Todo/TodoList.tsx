1104cdda6389f6f82ed4fe549c79c4fb6ee55473
2Kalnov <jdmcorolla21@gmail.com>
2021-01-10 23:23:29 +0300

    TODO list prototype

---
+++ View/todo-app/src/components/Todo/TodoList.tsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import {getAllTasks} from "../../api";
+import {Task} from "../../types/Task";
+import {TodoItem} from "./TodoItem";
+
+type TodoListProps = {
+
+}
+
+export const TodoList: React.FC<TodoListProps> = (props) => {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    getAllTasks().then(response => {
+      response.json().then(tasks => setTasks(tasks))
+    })
+  })
+
+  return (
+    <div>
+      {
+        tasks.map((task: Task) => {
+          return <TodoItem title={task.title} description={task.description} status={task.status} />
+        })
+      }
+    </div>
+  )
+
+}
