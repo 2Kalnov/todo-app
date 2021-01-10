22ee900cd81191f2cba530f79e5688db4994dad3
2Kalnov <jdmcorolla21@gmail.com>
2021-01-10 22:35:32 +0300

    Migration to create updatedAt and createdAt columns in Tasks table

---
+++ nest-server/migrations/1610305832419-AddTimestmapsToTasks.ts
@@ -0,0 +1,21 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export class AddTimestampsToTasks1610305832419 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('tasks', new TableColumn({
+            name: 'createdAt',
+            type: 'date',
+            default: 'CURRENT_TIMESTAMP'
+        }));
+
+        await queryRunner.addColumn('tasks', new TableColumn({
+            name: 'updatedAt',
+            type: 'date',
+            default: 'CURRENT_TIMESTAMP'
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+}
