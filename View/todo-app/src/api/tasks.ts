11560bdf9e6044fc20fac9242c91e29a2daa1ac4
2Kalnov <jdmcorolla21@gmail.com>
2021-01-11 22:42:54 +0300

    API method for POSTing task

--- View/todo-app/src/api/tasks.ts
+++ View/todo-app/src/api/tasks.ts
@@ -1,7 +1,16 @@
+import {CreateTaskDto} from "../types/CreateTaskDto";
+
 const BASE_URL = 'http://localhost:8080/tasks';
 
 export const getAllTasks = () => {
   return fetch(`${BASE_URL}/byStatus`, {
     method: 'GET'
+  })
+}
+
+export const addTask = (taskDto: CreateTaskDto) => {
+  return fetch(`${BASE_URL}`, {
+    method: 'POST',
+    body: JSON.stringify(taskDto)
   })
 }
