154f60689d0979210435426293844ebe86ec1b80
2Kalnov <jdmcorolla21@gmail.com>
2021-01-11 23:00:37 +0300

    Form for task creation

---
+++ View/todo-app/src/components/Todo/TodoItemForm.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import {TodoItemCard} from "./TodoItemCard";
+import {Button, makeStyles, TextField} from "@material-ui/core";
+import {TodoContainer} from "./TodoContainer";
+import {addTask} from "../../api/tasks";
+
+const useStyles = makeStyles(() => ({
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: '2.5rem',
+    backgroundColor: '#eeeeee',
+    paddingTop: '20px',
+    paddingBottom: '20px'
+  },
+  descriptionField: {
+    marginTop: '14px'
+  },
+  addButton: {
+    alignSelf: 'flex-end',
+    fontWeight: 'bold',
+    marginTop: '14px',
+    color: '#3f51b5'
+  }
+}))
+
+type TodoItemFormProps = {
+
+}
+
+export const TodoItemForm: React.FC<TodoItemFormProps> = (props) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const styles = useStyles()
+
+  const handleTitleChange = (event: any) => {
+    setTitle(event.target.value)
+  };
+
+  const handleDescriptionChange = (event: any) => {
+    setDescription(event.target.value)
+  };
+
+  const handleAddButtonClick = () => {
+    addTask({
+      title,
+      description
+    })
+    setTitle('')
+    setDescription('')
+  }
+
+  return (
+    <TodoItemCard className={styles.card}>
+      <TextField label='Заголовок' value={title} onChange={handleTitleChange} variant='standard'/>
+      <TextField
+        label='Что надо сделать?'
+        value={description}
+        onChange={handleDescriptionChange}
+        variant='standard'
+        className={styles.descriptionField}
+        multiline
+        rows={3}
+      />
+      <Button className={styles.addButton} onClick={handleAddButtonClick}>Добавить</Button>
+    </TodoItemCard>
+  )
+}
