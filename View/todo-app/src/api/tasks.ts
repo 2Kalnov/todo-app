3c19d5f80b396861c1d96f113c50e8f83e33b680
2Kalnov <jdmcorolla21@gmail.com>
2021-01-12 11:19:23 +0300

    API method for permanent task removing

--- View/todo-app/src/api/tasks.ts
+++ View/todo-app/src/api/tasks.ts
@@ -14,3 +14,7 @@ export const addTask = (taskDto: CreateTaskDto) => {
 export const completeTask = (taskId: number) => {
   return api.patch(`${BASE_URL}/${taskId}/done`)
 }
+
+export const removeTask = (taskId: number) => {
+  return api.delete(`${BASE_URL}/${taskId}`)
+}
